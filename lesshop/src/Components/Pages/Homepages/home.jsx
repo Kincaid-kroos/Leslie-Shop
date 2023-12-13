089217aad7b5e45f0c163a24e191d4093c410ec1
kincaid <kiprotichkincaid@gmail.com>
2023-12-13 19:46:01 +0300

    latest update

--- lesshop/src/Components/Pages/Homepages/home.jsx
+++ lesshop/src/Components/Pages/Homepages/home.jsx
@@ -176,9 +176,11 @@ const Home = () => {
           <div className="flex flex-col items-center justify-center p-6 space-y-6 rounded-lg  md:w-1/3">
             <img src="https://images.pexels.com/photos/1181686/pexels-photo-1181686.jpeg" className="-mt-14 w-20 h-20 rounded-lg" alt="" />
             <h5 className="text-lg font-bold text-[#ff3f19]">Ann Kandie</h5>
-            <p className="text-sm text-[#ffffff]">
-              “CVV Shop has supercharged my life. Right now i own a few businesses in town
-              and i&apos;m driving my first car”
+            <p className="text-md  text-[#ffffff]">
+            “CVV Shop is the ultimate high tech modern shop. The ability to make the world a little place
+              in terms of the currency access status is what is the most amazing thing. Would recommend 
+              anyone all day”
+             
             </p> 
           </div>
 
@@ -186,7 +188,7 @@ const Home = () => {
           <div className="hidden flex-col items-center p-6 space-y-6 rounded-lg  md:flex md:w-1/3">
             <img src="https://images.pexels.com/photos/6311585/pexels-photo-6311585.jpeg" className="-mt-14 w-20 h-20 rounded-lg" alt="" />
             <h5 className="text-lg font-bold text-[#ff3f19]">Ali Hussein</h5>
-            <p className="text-sm text-[#ffffff]">
+            <p className="text-md text-[#ffffff]">
               “We have been able to cancel so many other subscriptions since
               using CVV Shop. There is no more cross-channel confusion and
               everyone is much more focused.”
@@ -197,20 +199,20 @@ const Home = () => {
           <div className="hidden flex-col items-center p-6 space-y-6 rounded-lg  md:flex md:w-1/3">
             <img src="https://images.pexels.com/photos/428340/pexels-photo-428340.jpeg" className="-mt-14 w-20 h-20 rounded-lg" alt="" />
             <h5 className="text-lg font-bold text-[#ff3f19]">Richard Martin</h5>
-            <p className="text-sm text-[#ffffff]">
-              “CVV Shop is the ultimate high tech shop. The ability to make the world a little place
-              in terms of the currency access status is what is the most amazing thing. Would recommend 
-              anyone all day”
+            <p className="text-md text-[#ffffff]">
+            “CVV Shop has supercharged my life. Right now i own a few businesses in town
+              and i&apos;m driving my first car”
+             
             </p>
           </div>
         </div>
         
-        <div className="my-16 p-12">
+        <div className="my-16 p-12 hover:animate-bounce">
           <NavLink
             to="/shop"
-            className="p-3 px-6 text-white bg-[#ff3f19] rounded animate-bounce font-semibold
+            className="p-3 px-6 text-white bg-[#ff3f19] rounded font-semibold
              no-underline hover:bg-[#b7db37]"
-            >Get Started Today</NavLink >
+            >Get Started</NavLink >
         </div>
 
       </div>
